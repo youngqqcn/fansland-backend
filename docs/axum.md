f162208bc6323c41d5c6e50e2e774bef23a3fd9c
yqq <youngqqcn@gmail.com>
2024-01-15 14:51:48 +0800

    hello world

---
+++ docs/axum.md
@@ -0,0 +1,7 @@
+
+https://github.com/joelparkerhenderson/demo-rust-axum
+
+
+
+jwt: https://github.com/tokio-rs/axum/blob/main/examples/jwt/src/main.rs
+
--- src/api/main.rs
+++ src/api/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, API!");
+use axum::{routing::get, Router, Json};
+
+#[tokio::main]
+async fn main() {
+    // println!("Hello, API!");
+    let app = Router::new().route("/", get(handler));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> Json<&'static str>{
+    Json("hello world")
 }
