24f812335cd31984aabd31f763cdf5aeeb5105a3
yqq <youngqqcn@gmail.com>
2024-04-20 19:06:00 +0800

    注释掉签名

--- fansland_openapi/src/handler.rs
+++ fansland_openapi/src/handler.rs
@@ -18,58 +18,58 @@ pub async fn get_nft_ticket_qrcode(
     // State(app_state): State<AppState>,
     JsonReq(req): JsonReq<QueryTicketQrCodeReq>,
 ) -> Result<Response<Body>, (StatusCode, Json<RespVO<String>>)> {
-    let okx_salt = "ca17a3e225a85a74290831f504aceec5";
-    let sig_msg = req.chain_id.to_string()
-        + "&"
-        + &req.nft_contract
-        + "&"
-        + &req.nft_owner
-        + "&"
-        + &req.nft_token_id.to_string()
-        + "&"
-        + &req.timestamp.to_string()
-        + "&"
-        + okx_salt;
+    // let okx_salt = "ca17a3e225a85a74290831f504aceec5";
+    // let sig_msg = req.chain_id.to_string()
+    //     + "&"
+    //     + &req.nft_contract
+    //     + "&"
+    //     + &req.nft_owner
+    //     + "&"
+    //     + &req.nft_token_id.to_string()
+    //     + "&"
+    //     + &req.timestamp.to_string()
+    //     + "&"
+    //     + okx_salt;
 
-    // 接口签名验签
-    let now_ts = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap_or_default()
-        .as_secs();
+    // // 接口签名验签
+    // let now_ts = SystemTime::now()
+    //     .duration_since(UNIX_EPOCH)
+    //     .unwrap_or_default()
+    //     .as_secs();
 
-    // 正负60秒， 即时间窗口有120s
-    if now_ts.abs_diff(req.timestamp) > 60 {
-        tracing::error!(
-            "====时间戳无效=====系统当前时间戳: {}, 参数时间戳: {}",
-            now_ts,
-            req.timestamp
-        );
-        return Err((
-            StatusCode::BAD_REQUEST,
-            Json(RespVO::<String> {
-                code: Some(10002),
-                msg: Some("invalid timestamp".to_owned()),
-                data: None,
-            }),
-        ));
-    }
+    // // 正负60秒， 即时间窗口有120s
+    // if now_ts.abs_diff(req.timestamp) > 60 {
+    //     tracing::error!(
+    //         "====时间戳无效=====系统当前时间戳: {}, 参数时间戳: {}",
+    //         now_ts,
+    //         req.timestamp
+    //     );
+    //     return Err((
+    //         StatusCode::BAD_REQUEST,
+    //         Json(RespVO::<String> {
+    //             code: Some(10002),
+    //             msg: Some("invalid timestamp".to_owned()),
+    //             data: None,
+    //         }),
+    //     ));
+    // }
 
-    tracing::debug!("sig_msg is {}", sig_msg);
-    let h = digest(sig_msg.to_lowercase());
-    tracing::debug!("signatrue is {}", h);
+    // tracing::debug!("sig_msg is {}", sig_msg);
+    // let h = digest(sig_msg.to_lowercase());
+    // tracing::debug!("signatrue is {}", h);
 
-    if req.signature != h {
-        tracing::error!("====签名不匹配: 期望: {} , 实际: {}", h, req.signature);
+    // if req.signature != h {
+    //     tracing::error!("====签名不匹配: 期望: {} , 实际: {}", h, req.signature);
 
-        return Err((
-            StatusCode::BAD_REQUEST,
-            axum::Json(RespVO::<String> {
-                code: Some(10000),
-                msg: Some(String::from("invalid signature")),
-                data: None,
-            }),
-        ));
-    }
+    //     return Err((
+    //         StatusCode::BAD_REQUEST,
+    //         axum::Json(RespVO::<String> {
+    //             code: Some(10000),
+    //             msg: Some(String::from("invalid signature")),
+    //             data: None,
+    //         }),
+    //     ));
+    // }
     let mut fansland_nft_contract_address = req.nft_contract.to_lowercase();
 
     // FIX: for fix , 之前BSC早鸟票使用了大写, 使用这种方式修补, 2024-3-25 by yqq
@@ -91,11 +91,18 @@ pub async fn get_nft_ticket_qrcode(
 
     let r = QueryTicketQrCodeResp {
         chain_id: req.chain_id,
-        qrcode: qrcode,
-        nft_owner: req.nft_owner,
-        nft_contract: req.nft_contract,
+        qrcode: qrcode.clone(),
+        nft_owner: req.nft_owner.clone(),
+        nft_contract: req.nft_contract.clone(),
         nft_token_id: req.nft_token_id,
     };
+
+    tracing::info!(
+        "{},{},{}",
+        req.nft_token_id,
+        req.nft_owner,
+        qrcode,
+    );
 
     Ok(RespVO::from(&r).resp_json())
 }
